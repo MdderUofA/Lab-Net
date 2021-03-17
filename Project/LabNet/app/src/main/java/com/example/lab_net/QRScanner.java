b441032c928131c6fb508464752ea60e22010055
Marcus <mdder@ualberta.ca>
2021-03-17 04:09:14 -0600

    QR Scanner showing

---
+++ Project/LabNet/app/src/main/java/com/example/lab_net/QRScanner.java
@@ -0,0 +1,69 @@
+package com.example.lab_net;
+
+import android.Manifest;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.zxing.Result;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+// much of this base code was created by Amardeep https://stackoverflow.com/users/7035327/amardeep
+// source: https://stackoverflow.com/a/40103682
+public class QRScanner extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+
+    private ZXingScannerView scannerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr_scanner);
+        scannerView = findViewById(R.id.qr_scanner);
+
+        // Android Development Tutorial - Read QR Code with ZXing
+        // https://www.youtube.com/watch?v=MegowI4T_L8
+        Dexter.withActivity(this)
+                .withPermission(Manifest.permission.CAMERA)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+                        scannerView.setResultHandler(QRScanner.this);
+                        scannerView.startCamera();
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+                        Toast.makeText(QRScanner.this,"Permissions required to use QR",Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+
+                    }
+                }).check();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        scannerView.setResultHandler(this);
+        scannerView.startCamera();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        scannerView.stopCamera();
+    }
+
+    @Override
+    public void handleResult(Result result) {
+    }
+}
