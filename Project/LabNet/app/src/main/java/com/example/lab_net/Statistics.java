be15f0f2bbb325dc9904fa1f3cdebc033a4a9821
Vidhi-H-Patel <54190554+Vidhi-H-Patel@users.noreply.github.com>
2021-03-29 16:07:57 -0600

    Updated Statistics

--- Project/LabNet/app/src/main/java/com/example/lab_net/Statistics.java
+++ Project/LabNet/app/src/main/java/com/example/lab_net/Statistics.java
@@ -2,22 +2,23 @@ package com.example.lab_net;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class Statistics extends AppCompatActivity {
 
-    private TextView meanView, medianView, lowerQuartileView;
+    private TextView meanView, medianView, lowerQuartileView,upperQuartileView,standardDeviationView;
 
     private float sum = 0;
-    private double mean = 0, median = 0;
+    private double mean = 0;
+    private double median = 0;
     private double lowerQuartile = 0;
     private double upperQuartile = 0;
     private double standardDeviation = 0;
@@ -33,8 +34,7 @@ public class Statistics extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.stats_dialog);
-        meanView = (TextView) findViewById(R.id.mean_view);
+        setContentView(R.layout.activity_stats);
 
         //get from ExperimentActivity
         Intent intent = getIntent();
@@ -42,6 +42,7 @@ public class Statistics extends AppCompatActivity {
         results = (ArrayList<Long>) intent.getSerializableExtra("resultList");
         expId = intent.getStringExtra("ExperimentId");
 
+        //calculate mean
         for (i = 0; i < results.size(); i++) {
             sum = results.get(i) + sum;
         }
@@ -52,10 +53,10 @@ public class Statistics extends AppCompatActivity {
         }
         sum = 0;
 
-        doneButton = (Button) findViewById(R.id.doneButton);
-        meanView.setText("Mean: " + mean);
+        meanView = (TextView) findViewById(R.id.mean_value);
+        meanView.setText(String.format("%.2f",mean));
 
-        // median
+        //calculate median
         Collections.sort(results);
         int mid = results.size() / 2;
 
@@ -65,38 +66,78 @@ public class Statistics extends AppCompatActivity {
         else {
             median = results.get(mid);
         }
+        medianView = findViewById(R.id.median_value);
+        medianView.setText(String.valueOf(median));
 
-/*        int lq = results.size()/4;
-        if(results.size() % 2 == 0 && results.size() != 1)  {
-            lowerQuartile = (results.get(lq - 1) + results.get(lq))/2;
+        //calculate lower quartile
+        Collections.sort(results);
+        ArrayList<Long> lqArray = new ArrayList<>();
+        int l;
+        for (l = 0; l < mid; l++) {
+            lqArray.add(results.get(l));
         }
-        else if(results.size() == 1){
-            lowerQuartile = results.get(0);
+        int lq = lqArray.size()/2;
+        if (lqArray.size() % 2 == 0){
+            lowerQuartile = (lqArray.get(lq-1) + lqArray.get(lq)) / 2;
         }
-        else{
-            lowerQuartile = results.get(lq - 1);
+        else {
+            lowerQuartile = lqArray.get(lq);
         }
-
-        int uq = ((3 * results.size())/4);
-        if(results.size() % 2 == 0 && results.size() != 1)  {
-            upperQuartile = (results.get(uq - 1) + results.get(uq+1))/2;
-        }
-        else if(results.size() == 1){
-            upperQuartile = results.get(0);
-        }
-        else{
-            lowerQuartile = results.get(uq + 1);
-        }*/
-
-
-
-        medianView = findViewById(R.id.median_view);
-        medianView.setText("Median: " + median);
 
         lowerQuartileView = findViewById(R.id.q1);
+        lowerQuartileView.setText(String.valueOf(lowerQuartile));
 
-        lowerQuartileView.setText("Lower Quartile: " + lowerQuartile);
+        //calculate upper quartile
+        Collections.sort(results);
+        ArrayList<Long> uqArray = new ArrayList<>();
+        int u;
+        if (results.size() % 2 == 0){
+            u = mid;
+        }
+        else{
+            u = mid + 1;
+        }
 
+        for (; u < results.size(); u++) {
+            uqArray.add(results.get(u));
+        }
+        int uq = uqArray.size()/2;
+        if (uqArray.size() % 2 == 0){
+            upperQuartile = (uqArray.get(uq-1) + uqArray.get(uq)) / 2;
+        }
+        else {
+            upperQuartile = uqArray.get(uq);
+        }
+
+        upperQuartileView = findViewById(R.id.q3);
+        upperQuartileView.setText(String.valueOf(upperQuartile));
+
+        //calculate stdev
+        ArrayList<Double> stdevArray = new ArrayList<>();
+        for (i = 0; i < results.size();i++) {
+            double num = (results.get(i) - mean);
+            double x = Math.pow(num, 2);
+            stdevArray.add(x);
+        }
+        int j;
+        double stdevSum = 0;
+        for (j = 0; j < stdevArray.size();j++) {
+            stdevSum = stdevArray.get(j) + stdevSum;
+        }
+        if (stdevArray.size() != 0) {
+            double y = (stdevSum / (stdevArray.size()));
+            standardDeviation = Math.sqrt(y);
+        } else {
+            standardDeviation = 0;
+        }
+
+        System.out.println(stdevArray);
+        standardDeviationView = findViewById(R.id.stdev);
+        standardDeviationView.setText(String.format("%.2f",standardDeviation));
+
+
+
+        doneButton = (Button) findViewById(R.id.doneButton);
         doneButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
