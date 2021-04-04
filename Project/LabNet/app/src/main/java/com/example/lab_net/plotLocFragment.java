3862dad9a5ca59c918ae671819e394547751ec3e
Gurjog Singh <gurjog@ualberta.ca>
2021-04-05 01:50:58 +0300

    feature working

--- Project/LabNet/app/src/main/java/com/example/lab_net/plotLocFragment.java
+++ Project/LabNet/app/src/main/java/com/example/lab_net/plotLocFragment.java
@@ -22,15 +22,38 @@ import java.util.ArrayList;
 public class plotLocFragment extends Fragment {
 
     private static final String TAG = "TESTING";
-    private ArrayList<Double> locations;
+    private ArrayList<Double> latitude;
+    private ArrayList<Double> longitude;
+    private ArrayList<String> trialName;
+
+    public void putLocationOnMap(double latitude, double longitude, GoogleMap googleMap, String trialName){
+        //Help from 'How to Implement Google Map Inside Fragment in Android Studio | GoogleMap | Android Coding'
+        //by Android Coding (09/12/2020, YouTube) - https://www.youtube.com/watch?v=YCFPClPjDIQ
+
+        LatLng location = new LatLng(latitude, longitude);
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(location);
+        markerOptions.title(trialName);
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                location, 12
+        ));
+        googleMap.addMarker(markerOptions);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                location, 12
+        ));
+    }
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_plot_loc, container, false);
 
-        locations = (ArrayList<Double>) this.getArguments().getSerializable("locations");
-        Log.d(TAG, "onCreateView: TESTING" + locations.get(0));
+        latitude = (ArrayList<Double>) this.getArguments().getSerializable("latitude");
+        longitude = (ArrayList<Double>) this.getArguments().getSerializable("longitude");
+        trialName = (ArrayList<String>) this.getArguments().getSerializable("trialName");
+
         SupportMapFragment supportMapFragment = (SupportMapFragment)
                 getChildFragmentManager().findFragmentById(R.id.google_map);
 
@@ -41,19 +64,10 @@ public class plotLocFragment extends Fragment {
             @Override
             public void onMapReady(GoogleMap googleMap) {
 
-                googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-                    @Override
-                    public void onMapClick(LatLng latLng) {
-                        MarkerOptions markerOptions = new MarkerOptions();
-                        markerOptions.position(latLng);
-                        markerOptions.title(latLng.latitude + " : " + latLng.longitude);
-                        googleMap.clear();
-                        googleMap.addMarker(markerOptions);
-                        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
-                                latLng, 15
-                        ));
-                    }
-                });
+               for (int i = 0; i < (trialName.size()); i++ ){
+                   Log.d(TAG, "onMapReady: PLOTTING" + latitude.get(i) + longitude.get(i));
+                   putLocationOnMap(latitude.get(i), longitude.get(i), googleMap, trialName.get(i));
+               }
             }
         });
         return view;
