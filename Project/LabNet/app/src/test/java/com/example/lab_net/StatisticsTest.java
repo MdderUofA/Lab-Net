09779e605cc61963d83e1143e287389f5e7a23dd
Vidhi-H-Patel <54190554+Vidhi-H-Patel@users.noreply.github.com>
2021-04-09 15:32:05 -0600

    updated test cases

---
+++ Project/LabNet/app/src/test/java/com/example/lab_net/StatisticsTest.java
@@ -0,0 +1,50 @@
+package com.example.lab_net;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.junit.Assert.*;
+
+public class StatisticsTest {
+
+    @Test
+    public void testCalculateMedian() {
+        ArrayList<Double> list = new ArrayList<>(Arrays.asList(5.6,4.5,6.7,7.8,3.0));
+        Collections.sort(list);
+        double median = 0;
+        int mid = list.size() / 2;
+        if(list.size() % 2 != 0){
+            median = list.get(mid);
+        }
+        else{
+            median = (list.get(mid - 1) + list.get(mid)) / 2.0;
+        }
+
+        assertEquals(median, 5.6, 0;
+
+    }
+
+    @Test
+    public void testCalculateMean() {
+        ArrayList<Double> list = new ArrayList<>(Arrays.asList(5.6,4.5,6.7,7.8,3.0));
+        int i;
+        double sum = 0;
+        double mean = 0;
+        for (i = 0; i < list.size(); i++) {
+            sum = list.get(i) + sum;
+        }
+        if (list.size() != 0) {
+            mean = (sum / (list.size()));
+        } else {
+            mean = 0;
+        }
+
+        assertEquals(mean, 5.52);
+
+
+    }
+
+}
